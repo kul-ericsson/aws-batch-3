0c1f1aa503aa153af9887e5c2b27585550ad9ccf
Kulbhushan <kulbhushan.mayer@>
2020-12-07 13:24:22 +0530

    Create and new EC@ on newly created VPC with SG

---
+++ ec2_instance.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "kul_server" {
+  ami = "ami-01e36b7901e884a10"
+  instance_type = "t2.micro"
+  key_name = "kul-ericsson"
+  tags = {
+    "Name" = "${var.tagname}_Server"
+  }
+  subnet_id = aws_subnet.public_sn.id
+  vpc_security_group_ids = [ aws_security_group.kul_sg.id ]
+}

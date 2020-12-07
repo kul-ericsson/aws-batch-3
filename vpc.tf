308ee898bd81ed5c5173ca7da45d40a4d5224518
Kulbhushan <kulbhushan.mayer@>
2020-12-07 12:04:59 +0530

    Added Subnets 1 Public & 2 Private

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,30 @@ resource "aws_vpc" "kul_vpc" {
      Name = var.tagname
  }
 }
+
+resource "aws_subnet" "public_sn" {
+  vpc_id = aws_vpc.kul_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  tags = {
+    "Name" = "${var.tagname}_public_sn"
+  }
+}
+
+resource "aws_subnet" "private_sn_1" {
+  vpc_id = aws_vpc.kul_vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "false"
+  tags = {
+    "Name" = "${var.tagname}_private_sn_1"
+  }
+}
+
+resource "aws_subnet" "private_sn_2" {
+  vpc_id = aws_vpc.kul_vpc.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = "false"
+  tags = {
+    "Name" = "${var.tagname}_private_sn_2"
+  }
+}
